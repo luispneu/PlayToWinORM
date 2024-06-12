5ba663ea70954f8cbb1cda4a1893cd8e721dbc6b
luis11! <131817446+luispneu@users.noreply.github.com>
2024-06-12 10:48:37 -0400

    Add files via upload

---
+++ PlayToWinORM_Luis-main/index.js
@@ -0,0 +1,82 @@
+// Importações de módulos e configurações
+require("dotenv").config();
+const express = require("express");
+const conn = require("./db/conn");
+const Jogo = require("./models/Jogo");
+const handlebars = require("express-handlebars");
+const { json } = require("express/lib/response");
+const Usuario = require("./models/Usuario");
+const res = require("express/lib/response");
+
+const app = express();
+
+// HandleBars
+app.engine("handlebars", handlebars.engine())
+app.set("view engine", "handlebars")
+
+// Configurações do middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Rotas
+app.post("/jogo/novo", async (req, res) => {
+  const dadosJogo = {
+    titulo: req.body.titulo,
+    descricao: req.body.descricao,
+    precoBase: req.body.precoBase
+  }
+
+  const jogo = await Jogo.create(dadosJogo);
+  res.send("Jogo cadastrado com id" + jogo.id);
+});
+
+
+app.post("/usuario/novo", async (req, res) => {
+  const dadosUsuario = {
+    nickname: req.body.nickname,
+    nome: req.body.nome
+  }
+
+  const usuario = await Usuario.create(dadosUsuario);
+  res.send("Usuario cadastrado com id" + usuario.id);
+});
+
+app.get("/jogo/novo", (req, res) => {
+  res.render(`formJogo`);
+});
+
+app.get("/usuarios/novo", (req, res) => {
+  res.render(`formUsuario`);
+});
+
+
+app.get("/", (req, res) => {
+  res.render(`home`);
+});
+
+app.get("/usuarios", async (req, res) => {
+  const usuarios = await Usuario.findAll({ raw: true });
+  res.render("usuarios", { usuarios })
+})
+
+app.get("/usuarios/:id/atualizar", async (req, res) => {
+  const id = req.params.id
+  const usuario = await Usuario.findByPk(id, { raw: true })
+  res.render("formUsuario", { usuario });
+})
+
+// Inicialização do servidor
+app.listen(8000, () => {
+  console.log("Abridu!");
+  console.log("http://localhost:8000/")
+});
+
+// Conexão com o banco de dados
+conn
+  .sync()
+  .then(() => {
+    console.log("Conectado ao banco de dados com sucesso!");
+  })
+  .catch((err) => {
+    console.log("Ocorreu um erro: " + err);
+  });
---
+++ PlayToWinORM_Luis-main/models/Jogo.js
@@ -0,0 +1,25 @@
+const db = require('../db/conn');
+const {DataTypes} = require("sequelize");
+
+const Jogo = db.define("Jogo",{
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+    },
+    titulo:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    descricao:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    precoBase:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }
+});
+
+module.exports = Jogo
